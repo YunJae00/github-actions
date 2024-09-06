69485ec02e3215edc104aeea3c46c991787fcded
YunJae00 <0109548@naver.com>
2024-09-07 04:11:55 +0900

    test end

--- src/main/java/com/example/githubactions/controller/testController.java
+++ src/main/java/com/example/githubactions/controller/testController.java
@@ -6,31 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "test";
-    }
-
-    @GetMapping("/test2")
-    public String test2() {
-        return "test2";
-    }
-
-    @GetMapping("/test3")
-    public String test3() {
-        return "test3";
-    }
-
-    @GetMapping("/test4")
-    public String test4() {
-        return "test4 and 5";
-    }
-
-    @GetMapping("/test5")
-    public String test5() {
-        return "and 5";
-    }
-
     @GetMapping("/test6")
     public String test6() {
         return "test 6";
