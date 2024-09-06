1518c6a29a96ff8a60bd4b51d95571b02a4ab29e
YunJae00 <0109548@naver.com>
2024-09-07 03:57:36 +0900

    all change

--- scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 기존 스크립트 삭제
-rm -f /home/ubuntu/app/scripts/start.sh
-rm -f /home/ubuntu/app/scripts/stop.sh
-rm -f /home/ubuntu/app/scripts/cleanup.sh
-
-echo "Old scripts deleted."
